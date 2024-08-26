d6b0be6ef2db3555af28d6196a5e1836a8448cd4
Diegooz <85436549+Pixelssquad@users.noreply.github.com>
2024-08-26 02:48:22 +0200

    Update scratch.ino

--- scratch.ino
+++ scratch.ino
@@ -1,1 +1,22 @@
+#define EXTLED 13
+#define INTLED 2
+#define REDLED 25
+
+void setup() {
+  pinMode(INTLED,OUTPUT);
+  pinMode(EXTLED,OUTPUT);
+  pinMode(REDLED,OUTPUT);
+
+}
+void loop() {
+  delay(1000);
+  digitalWrite(INTLED,HIGH);
+  digitalWrite(EXTLED,LOW);
+  digitalWrite(REDLED,HIGH);  
+  delay(1000);
+  digitalWrite(INTLED,LOW); 
+  digitalWrite(EXTLED,HIGH);   
+  digitalWrite(REDLED,LOW);   
+
+}
 
